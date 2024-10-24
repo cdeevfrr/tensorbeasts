9b53fc9379aac9870cd927b513068b5ffadc5026
Danny Rivers <cdeevfrr@gmail.com>
2024-10-24 15:30:21 -0400

    Prevents fleeing battles

--- Code/app/_layout.tsx
+++ Code/app/_layout.tsx
@@ -4,7 +4,8 @@ export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: 'Home' }} />
-      <Stack.Screen name="battle" options={{ title: 'Battle' }}/>
+      <Stack.Screen name="battle" options={{ title: 'Battle', headerShown: false }}/>
+      <Stack.Screen name="dungeon" options={{ title: 'Dungeon' }}/>
       <Stack.Screen name="beasts" options={{ title: 'Beasts' }}/>
       <Stack.Screen name="parties" options={{ title: 'Parties' }}/>
     </Stack>
