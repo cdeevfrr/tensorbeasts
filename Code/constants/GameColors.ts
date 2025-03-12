2b1df49472115955f1650acf78727e52d44dfba7
Danny Rivers <cdeevfrr@gmail.com>
2025-03-12 13:54:21 -0400

    Comments what color each game color is

--- Code/constants/GameColors.ts
+++ Code/constants/GameColors.ts
@@ -5,32 +5,32 @@ interface GameColor {
 }
 
 export const GameColors = {
-    1: {
+    1: { // Purple
         text: '#11181C',
         background: '#663399',
         border: '#16171b'
     } as GameColor,
-    2: {
+    2: { // Blue
         text: '#11181C',
         background: '#3366ff',
         border: '#002080'
     } as GameColor,
-    3: {
+    3: { // Red
         text: '#11181C',
         background: '#990033',
         border: '#4d0019'
     } as GameColor,
-    4: {
+    4: { // Green
         text: '#11181C',
         background: '#00e64d',
         border: '#006622'
     } as GameColor,
-    5: {
+    5: { // Orange
         text: '#11181C',
         background: '#ff9900',
         border: '#995c00'
     } as GameColor,
-    default: {
+    default: { // grey
         text: '#11181C',
         background: '#808080',
         border: '#0f0f0f'
