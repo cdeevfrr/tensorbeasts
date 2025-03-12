868c1cd5d283499edaa4e3b8bf96d592572757c1
Danny Rivers <cdeevfrr@gmail.com>
2025-03-12 14:40:43 -0400

    Makes a correct timeBaby image

--- Code/Game/BeastDex/Images/TimeBaby.tsx
+++ Code/Game/BeastDex/Images/TimeBaby.tsx
@@ -1,63 +1,70 @@
 import { GameColors } from "@/constants/GameColors";
-import Svg, { Circle, Ellipse, Path } from "react-native-svg";
+import Svg, { Circle, Ellipse, Path, Polyline, Rect } from "react-native-svg";
 
 export function TimeBaby({
-    background, 
-    foreground, 
-    border
+    colors
 }: {
-    background: keyof typeof GameColors, 
-    foreground: keyof typeof GameColors,
-    border: keyof typeof GameColors,
+    colors: Array<keyof typeof GameColors>
 }) {
+    const color1 = colors[0] || 'default'
+    const color2 = colors[1] || color1
+    const color3 = colors[2] || color2
+    const color4 = colors[3] || color3
     return (
-        <Svg viewBox="0 0 100 100">
+        // Settings when building this on boxy-svg.com:
+        // viewBox="152.591 292.327 113.929 87.225" width="113.929px" height="87.225px"
+        <Svg viewBox="100 200 400 400">
             {/* Body */}
             <Ellipse
-                cx="50"
-                cy="60"
-                rx="25"
-                ry="30"
-                fill={GameColors[background].background}
-                stroke={GameColors[border].border}
-                strokeWidth="2"
+                fill={GameColors[color1].background}
+                cx="209.942"
+                cy="292.755"
+                rx="57.198"
+                ry="53.272"
             />
-            {/* Head */}
-            <Circle 
-                cx="50"
-                cy="40"
-                r="15"
-                fill={GameColors[background].background}
-                stroke={GameColors[border].border}
-                strokeWidth="2"
-            />
-            {/* Eye */}
-            <Circle 
-                cx="55"
-                cy="35"
-                r="3"
-                fill={GameColors[foreground].text}
-            />
-            {/* Beak */}
-            <Path 
-                d="M 60 38 Q 70 40, 60 42"
-                fill="none"
-                stroke={GameColors[foreground].text}
-                strokeWidth="2"
-            />
+            <Rect
+                fill={GameColors[color1].background}
+                x="152.591"
+                y="292.327"
+                width="113.929"
+                height="87.225" />
+            {/* Eyes */}
+            <Ellipse 
+              cx="181.705" cy="271.13" rx="5.621" ry="6.02"
+              fill={GameColors[color2].background}
+              strokeWidth="2"
+              stroke={GameColors[color2].border}/>
+            <Ellipse 
+              cx="256.339" cy="271.26" rx="4.989" ry="5.55"
+              fill={GameColors[color2].background}
+              strokeWidth="2"
+              stroke={GameColors[color2].border}/>
             {/* Feet */}
-            <Path 
-                d="M 40 85 Q 45 90, 50 85"
-                fill="none"
-                stroke={GameColors[border].border}
+            <Ellipse 
+              cx="249.577" cy="381.318" rx="37.958" ry="5.628" 
+              fill={GameColors[color2].background}
+              strokeWidth="2"
+              stroke={GameColors[color2].border}/>
+            <Ellipse cx="189.387" cy="380.987" rx="37.958" ry="5.628" 
+              fill={GameColors[color2].background}
+              strokeWidth="2"
+              stroke={GameColors[color2].border}/>
+
+            {/* Clock */}
+            <Ellipse cx="219.32" cy="336.676" rx="37.234" ry="37.574" 
+                fill={GameColors[color3].background}
                 strokeWidth="2"
-            />
-            <Path 
-                d="M 50 85 Q 55 90, 60 85"
+                stroke={GameColors[color3].border}/>
+            <Polyline points="219.854,308.014 220.036,338.055 238.269,338.038" 
                 fill="none"
-                stroke={GameColors[border].border}
                 strokeWidth="2"
-            />
+                stroke={GameColors[color3].border}/>
+
+            {/* Beak */}
+            <Ellipse cx="251.781" cy="293.747" rx="36.817" ry="9.319" 
+            fill={GameColors[color4].background}
+            strokeWidth="2"
+            stroke={GameColors[color4].border}/>
         </Svg>
     );
 }
